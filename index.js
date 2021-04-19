218506a5957c1d7621e1c8c71c29ccab6d1220d0
kevin <kevin.voyer.developpeur@gmail.com>
2021-04-19 03:18:07 +0200

    feat: create function multiply

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ module.exports.sum = function(a, b) {
   return a + b;
 }
 
-
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+module.exports.multiply = function(a, b) {
+  return a * b;
+}
