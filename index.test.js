c0934eff24a8c0e773272194b6850be82abb17c8
kevin <kevin.voyer.developpeur@gmail.com>
2021-04-19 03:18:22 +0200

    feat: create tests for function multiply

--- index.test.js
+++ index.test.js
@@ -1,7 +1,11 @@
-const { sum } = require('./index');
+const { sum, multiply } = require('./index');
 
 describe('Test all function inside of index file', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+  });
+
+  test('multiply 2 * 2 to equal 4', () => {
+    expect(multiply(2, 2)).toBe(4);
   });
 });
