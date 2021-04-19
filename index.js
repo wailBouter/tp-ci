f97173eb77d708f571878d5ab86b7ecbf7b33a99
kevin <kevin.voyer.developpeur@gmail.com>
2021-04-19 03:20:34 +0200

    feat: create divide function

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ module.exports.sum = function(a, b) {
 module.exports.multiply = function(a, b) {
   return a * b;
 }
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns
+ */
+module.exports.divide = function(a, b) {
+  return a / b;
+}
